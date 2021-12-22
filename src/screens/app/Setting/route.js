b1f019dae64130e0896871a889bd0449c8ef92bd
Hoang Nguyen <hoang.nguyen@fruitful.io>
2021-12-22 10:53:13 +0700

    add feature: update user-profile-info

---
+++ src/screens/app/Setting/route.js
@@ -0,0 +1,22 @@
+import React, { lazy } from "react";
+import { initModules } from "router";
+import { RocketFilled, RocketOutlined } from "@ant-design/icons";
+import { v4 as uuidv4 } from 'uuid';
+
+export const exploreModule = { key: "setting", path: "Setting" };
+
+const exploreRoute = {
+  index: uuidv4(),
+  path: "/setting",
+  label: "Settings",
+  exact: true,
+  isPrivate: true,
+  isShowOnNav: false,
+  activeIcon: <RocketFilled />,
+  inActiveIcon: <RocketOutlined />,
+  component: lazy(async () => {
+    await initModules([exploreModule], "app");
+    return import(".");
+  }),
+};
+export default exploreRoute;
